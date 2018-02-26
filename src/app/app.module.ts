d42c87f2ae8b9f48756d219808cd87a6bbec1093
Eugenio Rangel <eugenio_rangel@hotmail.com>
2018-02-26 16:23:13 -0600

    se termino de implementar login falso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 //Componentes
 import { AppComponent } from './app.component';
---
+++ src/app/services/fake-backend.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest } from '@angular/common/http';
+@Injectable()
+export class FakeBackendService {
+
+  constructor() { }
+
+}

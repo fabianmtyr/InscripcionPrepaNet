e7fcc8696eeb5ee898fbf11eccebb2adee3f75e4
Eugenio Rangel <eugenio_rangel@hotmail.com>
2018-03-21 16:09:16 -0600

    Revert "Merge branch 'master' into registro-admins"
    This reverts commit 2ec8c3449e5b8cb04f30a0e13aa1b67f354a8f34, reversing
    changes made to 36dba0dfb9a8ea0b766b9cd410ee9f3877b986c2.

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,10 +1,7 @@
 export class User {
 	public id: number;
-<<<<<<< HEAD
 	public fname: string;
 	public lname: string;
-=======
->>>>>>> login-admin
 	public email: string;
 	public password: string;
 	public passwordVerify: string;
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@angular/core';
-import { User } from 'app/models/user.model';
+import { User } from '../models/user.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class UserService {
-<<<<<<< HEAD
   private backend = "https://ipn-backend.herokuapp.com";
-=======
-  protected backend: string;
-  protected headers: HttpHeaders;
-
->>>>>>> login-admin
 	private isUserLoggedIn;
-
-  apiRoot: string = "https://ipn-backend.heroku";
-  results: User;
+	private username;
 
   constructor(private http: HttpClient) { 
   	this.isUserLoggedIn = false;
-    this.results = new User;
-    this.backend = "https://ipn-backend.herokuapp.com";
   }
 
   setUserLoggedIn() {
@@ -31,31 +21,12 @@ export class UserService {
   	return this.isUserLoggedIn;
   }
 
-<<<<<<< HEAD
   login(user:User) {
     return this.http.post('liga de backend de user', user);
   }
 
   registerUser(user: User) {
     return this.http.post(this.backend + '/user/register', user);
-=======
-/*
-  loginP(userP: User) {
-    let promise = new Promise((resolve, reject) => {
-      let apiURL = `${this.backend}/user.login`;
-      this.http.get(apiURL).toPromise().then(
-        res => { 
-          console.log(res.json());
-          resolve();
-        }
-        );
-    });
-    return promise;
-  }
-*/
-  login(user: User) {
-    return this.http.post(this.backend + '/user/login', user);
->>>>>>> login-admin
   }
 
 }

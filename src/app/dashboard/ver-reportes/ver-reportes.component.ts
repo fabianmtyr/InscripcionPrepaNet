59ff5357d2accd01dfc5cc3bdc1e8d0487967fa1
bxmn1789 <boxman17@gmail.com>
2018-05-06 17:20:11 -0500

    Comentar Codigo y Borrar Console Logs etc

--- src/app/dashboard/ver-reportes/ver-reportes.component.ts
+++ src/app/dashboard/ver-reportes/ver-reportes.component.ts
@@ -21,8 +21,8 @@ import { DecimalPipe } from '@angular/common';
 })
 export class VerReportesComponent implements OnInit {
 
-      private rows: Array<any> = []
-      public length:number = 0;
+      private rows: Array<any> = [] //filas de tablas de reportes
+      public length:number = 0; //cantidad de filas
     
 
 	tutors:Observable<any> = this.http.get('https://ipn-backend.herokuapp.com/tutors/new');
@@ -41,107 +41,44 @@ export class VerReportesComponent implements OnInit {
 
 
   ngOnInit() {
-  	//console.log(this.dataSource)
+  	//Obtener datos de tutores y ordenar para desplegar en reporte de cursos (el default al abrir pagina)
   	this.tutors = this.http.get('https://ipn-backend.herokuapp.com/tutors/list');
   	this.tutorService.getAllTutors().subscribe((response) => {
                 this.rows = response
                 this.dataSource.data= response.sort((a, b) => {return ((b.calificacionCurso || -1*1) - (a.calificacionCurso || -1*1))})
-  		console.log(this.dataSource.data);
                 this.length = this.rows.length
   	});
   }
   
-  /*
-  refresh() {
-  	this.tutorService.getAllTutors().subscribe((response) => {
-  		this.dataSource.data = response;
-  		this.changeDetectorRefs.detectChanges();
-  	})
-  }*/
-
-  toggleElegibilidad(tutor) {
-  	tutor.isElegible = !tutor.isElegible;
-    this.tutorService.editTutor(tutor).subscribe(
-      (response) => {
-        console.log(response);
-        console.log("Se edito tutor exitosamente!");
-      },
-      (error) => {
-        console.log(error);
-        console.log("No se pudo enviar forma.");
-        tutor.isElegible = !tutor.isElegible;
-      });
-
-  }
-  
-    applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
-  }
-  
-    /*public downloadExcel(){
-    //this.svs.exportAsExcelFile(this.rows,"tutores")
-      console.log(this.rows)
-      let flat = {};
-      var pth=''
-      let x = this.dataSource.data.map((dt) => {
-      //let x =this.rows.map((dt) => {
-          delete dt['_id']
-    return this.flatten(dt);
-    
-    });
-      console.log(x)
-      this.svs.specialExport(x,"tutores")      
-  }*/
-  
-  //tabChangeEvent : MatTabChangeEvent
+  //Manejar cambio de vistas/reporte seleccionado
   tabChanged(tabChangeEvent){
-  console.log('tabChangeEvent => ', tabChangeEvent);
-  console.log('index => ', tabChangeEvent.index);
+      
+  //console.log('tabChangeEvent => ', tabChangeEvent);
+  //console.log('index => ', tabChangeEvent.index);
   
+  //Reporte de Calificacion de Cursos
   if(tabChangeEvent.index == 0){
       this.displayedColumns = ['matricula', 'courseGrade'];
       this.dataSource.data=this.rows;
-      
       this.dataSource.data.sort((a, b) => {return ((b.calificacionCurso || -1*1) - (a.calificacionCurso || -1*1))})
       this.dataSource._updateChangeSubscription()
       
-      //filter by courseGrade > passGrade
-            //this.dataSource.data
-      
   }
-  
+  //Reporte de Materias
   else if(tabChangeEvent.index == 1){
       this.displayedColumns = ['matricula', 'materia1', 'materia2', 'materia3'];
-      //this.displayedColumns = ['matricula', 'materias'];
       this.dataSource.data = this.rows.filter((tut) => {return (tut.calificacionCurso && tut.calificacionCurso > 80)})
         .sort((a, b) => {return ((b.calificacionCurso || -1*1) - (a.calificacionCurso || -1*1))})
-      console.log(this.rows)
       this.dataSource._updateChangeSubscription()
   }
-  
+  //Reporte de Promedios
   else {
       this.displayedColumns = ['matricula', 'promedio'];
-      //no filter, undo filter
       this.dataSource.data=this.rows;
       this.dataSource.data.sort((a, b) => {return ((b.promedio || -1*1) - (a.promedio || -1*1))})
       this.dataSource._updateChangeSubscription()
   }
   
   }
-  /*
-    sortData(sort) {
-    const data = this.rows.slice();
-    //const data = this.dataSource.data
-    if (!sort.active || sort.direction == '') {
-      this.sortedData = data;
-      return;
-    }
-  }
-
- compare(a, b, isAsc) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-}*/
 
 }
